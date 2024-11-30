05e19a0648671e45bc04a26036a7be60b8f2c116
Student <yllberisha67@gmail.com>
2024-11-30 03:46:03 -0800

    ...

---
+++ lession6.py
@@ -0,0 +1,23 @@
+from my_package import module1 as m1
+from my_package import module2 as m2
+from my_package import module3 as m3
+import emoji
+
+print(emoji.emojize("Python is fun :snake: "))
+
+m1.welcome()
+m2.greet()
+m3.hello()
+
+
+
+
+
+
+#from math import sqrt as sq
+#import importlib
+
+
+
+#result = sq(25)
+#print(result)
--- module5/lession5.py
+++ module5/lession5.py
@@ -1,1 +1,38 @@
-name = "yll"
+total = 0
+
+for number in range(1,11):
+    if number % 2 == 0:
+        total += number
+print("Shuma e numrave qift prej 1 deri ne 10 eshte:", total)
+
+def greet():
+    print("Hello World")
+
+greet()
+
+def greet_person(name):
+    print("Hello", name)
+
+greet_person("Filan")
+
+#result = add(3,8)
+#print("3 + 7=", result)
+
+greeting = "Hello"
+
+def greet(name):
+    message = f"{greeting}, {name}!"
+    print(message)
+greet("Michael")
+print(greeting)
+
+
+
+def greet_person(name, greeting="Hello"):
+    message = f"{greeting}, {name}!"
+    return message
+
+default_greeting = greet_person("Rian")
+custom_greeting = greet_person("Eris", "Hi")
+print(default_greeting)
+print(custom_greeting)
