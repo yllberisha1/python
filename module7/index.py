58e6402e50cddd8f08b6f3aaff96072f95a2e636
Student <yllberisha67@gmail.com>
2024-12-07 03:11:14 -0800

    ...

---
+++ module7/index.py
@@ -0,0 +1,27 @@
+from traceback import print_tb
+
+age = 17
+
+age_as_str =str(age)
+
+print(age_as_str, "type of", type(age_as_str))
+
+
+print(bool(0))
+print(bool(42))
+
+
+print(bool(""))
+print(bool("hello"))
+
+print(bool())
+print(bool())
+
+
+
+
+x = 85
+y = 9.5
+
+result = x+y
+print(result, "type of", type(result))
