944e796717c0ac0516840ddd240be95b4b6697ba
Student <yllberisha65@gmail.com>
2024-12-14 03:43:03 -0800

    ...

---
+++ index2.py
@@ -0,0 +1,27 @@
+def calculate(num1, num2, operator):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero"
+    else:
+        return "Invalid operator"
+
+
+result1 = calculate(10, 5, "+")
+result2 = calculate(10, 5, "-")
+result3 = calculate(10, 5, "*")
+result4 = calculate(10, 0, "/")
+result5 = calculate(10, 5, "%")
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
--- module7/error_handeling.py
+++ module7/error_handeling.py
@@ -1,17 +1,41 @@
-from lesson2.lesson2 import fruts
+#from logging import exception
 
 try:
-    result =  10/10
+    result = 10/10
+    print(result)
+except ZeroDivisionError:
+     error = "the number you are trying to devdie a number with 0"
+     print(error)
 
-except:
-    print("this is an error")
+fruits = {
+    "apple": 4,
+    "cherry": 3,
+    "banana": 5
 
-fruts = {
-    "apple":5,
-    "banana":7,
-    "pear":6,
 }
 try:
-    print(fruts[input("tell me a frut")])
+    print(fruits["banana"])
+except KeyError:
+    print("the fruit is not on the dictionary")
+
+
+
+text = ("this is a string")
+
+try:
+    print(int(text))
+
+except Exception as e:
+    print("there was an error while prasing the data ", e)
+
+
+
+try:
+    number =  10/3
+except:
+    print("the number you are trying to devide is not alivabile right now")
+else:
+    print("the devision  has gone sucessufuly  congrats and the result is: ", number)
+
 
 
