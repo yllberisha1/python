eb8c8852b8c09b2b2712200451856d983bef4601
Student <yllberisha65@gmail.com>
2025-01-25 03:47:51 -0800

    ...

--- module11/lession11.py
+++ module11/lession11.py
@@ -1,1 +1,43 @@
-name = "yll"
+import datetime
+
+currnet_datetime = datetime.datetime.now()
+
+print("year:", currnet_datetime.year)
+print("month:", currnet_datetime.month)
+print("day:", currnet_datetime.day)
+print("hour:", currnet_datetime.hour)
+print("minute:", currnet_datetime.minute)
+print("second:", currnet_datetime.second)
+
+
+currnet_date = datetime.datetime.now().date()
+
+print(currnet_date)
+
+settime_date = datetime.datetime.now().time()
+
+specific_time = datetime.date(2000, 2 ,3)
+
+print(specific_time.year)
+print(specific_time.month)
+print(specific_time.day)
+
+plusday = current_datet + datetime.datatime(days=100)
+print(plusday)
+
+text = "this is the text i\n "
+
+
+
+file_path = "example.txt"
+file = open(file_path"r")
+content = file.read(()
+         print(content)
+
+with open("exaample.txt","r") as file:
+    lines = file.redlins()
+    print(lines)
+
+
+
+
