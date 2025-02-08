b30ebecb5073371a5d9b3186a02e292236d381af
Student <yllberisha65@gmail.com>
2025-02-08 03:48:18 -0800

    ...

--- module13/lession13.py
+++ module13/lession13.py
@@ -1,1 +1,19 @@
-name = "ylli"
+from itertools import product
+
+import numpy as np
+
+import pandas as pd
+
+
+# arr_2d = np.array ([[1,2,3,4,5], [6,7,8,9,10]])
+# print(arr_2d)
+#
+# element = arr_2d[1,2]
+# print(element)
+
+product = ["apple", "peach", "cherry", "orange"]
+sales = [12, 40, 100, 120 ]
+
+product_seris = pd.series(sales, index=(product))
+
+total_series
