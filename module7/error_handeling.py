2fd8cf6eb2acf066a9bffbb2e298dab32ca36584
Student <yllberisha65@gmail.com>
2025-03-08 03:37:46 -0800

    ...

--- module7/error_handeling.py
+++ module7/error_handeling.py
@@ -1,41 +1,41 @@
-#from logging import exception
-
-try:
-    result = 10/10
-    print(result)
-except ZeroDivisionError:
-     error = "the number you are trying to devdie a number with 0"
-     print(error)
-
-fruits = {
-    "apple": 4,
-    "cherry": 3,
-    "banana": 5
-
-}
-try:
-    print(fruits["banana"])
-except KeyError:
-    print("the fruit is not on the dictionary")
-
-
-
-text = ("this is a string")
-
-try:
-    print(int(text))
-
-except Exception as e:
-    print("there was an error while prasing the data ", e)
-
-
-
-try:
-    number =  10/3
-except:
-    print("the number you are trying to devide is not alivabile right now")
-else:
-    print("the devision  has gone sucessufuly  congrats and the result is: ", number)
-
-
-
+# #from logging import exception
+#
+# try:
+#     result = 10/10
+#     print(result)
+# except ZeroDivisionError:
+#      error = "the number you are trying to devdie a number with 0"
+#      print(error)
+#
+# fruits = {
+#     "apple": 4,
+#     "cherry": 3,
+#     "banana": 5
+#
+# }
+# try:
+#     print(fruits["banana"])
+# except KeyError:
+#     print("the fruit is not on the dictionary")
+#
+#
+#
+# text = ("this is a string")
+#
+# try:
+#     print(int(text))
+#
+# except Exception as e:
+#     print("there was an error while prasing the data ", e)
+#
+#
+#
+# try:
+#     number =  10/3
+# except:
+#     print("the number you are trying to devide is not alivabile right now")
+# else:
+#     print("the devision  has gone sucessufuly  congrats and the result is: ", number)
+#
+#
+#
--- module7/index.py
+++ module7/index.py
@@ -1,47 +1,47 @@
-from traceback import print_tb
-
-from pyexpat.errors import messages
-
-age = 17
-
-age_as_str =str(age)
-
-print(age_as_str, "type of", type(age_as_str))
-
-
-print(bool(0))
-print(bool(42))
-
-
-print(bool(""))
-print(bool("hello"))
-
-print(bool())
-print(bool())
-
-
-
-
-x = 85
-y = 9.5
-
-result = x+y
-print(result, "type of", type(result))
-
-age = 22
-
-message = "im" +str(age) + "years old"
-print(message)
-
-
-
-a = 5
-b = "3"
-
-result1 = a * int(b)
-print(result1)
-
-
+# from traceback import print_tb
+#
+# from pyexpat.errors import messages
+#
+# age = 17
+#
+# age_as_str =str(age)
+#
+# print(age_as_str, "type of", type(age_as_str))
+#
+#
+# print(bool(0))
+# print(bool(42))
+#
+#
+# print(bool(""))
+# print(bool("hello"))
+#
+# print(bool())
+# print(bool())
+#
+#
+#
+#
+# x = 85
+# y = 9.5
+#
+# result = x+y
+# print(result, "type of", type(result))
+#
+# age = 22
+#
+# message = "im" +str(age) + "years old"
+# print(message)
+#
+#
+#
+# a = 5
+# b = "3"
+#
+# result1 = a * int(b)
+# print(result1)
+#
+#
 
 
 
